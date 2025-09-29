fe0082022fa3b87ab4e89495c09ca391118ef9d6
Dener Garcia <dener.criarbr@gmail.com>
2025-09-29 04:08:02 +0000

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // comment base line for deploy out github pages
-  base: '/game-quiz-start-machine/',
+  //base: '/game-quiz-start-machine/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
