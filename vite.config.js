2efbbd9a2ef4e8317c2ca37f2cbc4a35b806759a
Dener Garcia <dener.criarbr@gmail.com>
2025-09-29 14:21:36 +0000

    fix: remove assets to /assets

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // comment base line for deploy out github pages
-  base: '/game-quiz-start-machine',
+  base: '/game-quiz-start-machine/',
   build: {
     rollupOptions: {
       input: {
