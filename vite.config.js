fdca66c54cf7376ccf1404fd5a017fcf608f13f3
Dener Garcia <dener.criarbr@gmail.com>
2025-09-29 03:24:55 +0000

    chore: add vite bundle and create workflow to deploy github pages

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // comment base line for deploy out github pages
+  base: '/game-quiz-start-machine',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ceos: resolve(__dirname, 'src/pages/ceos.html'),
+        flip: resolve(__dirname, 'src/pages/flip-card.html'),
+        kaban: resolve(__dirname, 'src/pages/kaban1.html'),
+        maker: resolve(__dirname, 'src/pages/maker-machine.html'),
+        packer: resolve(__dirname, 'src/pages/packer-machine.html'),
+        quebra: resolve(__dirname, 'src/pages/quebra-cabeca.html'),
+        rpg: resolve(__dirname, 'src/pages/rpg-factory.html')
+      },
+    },
+  },
+})
